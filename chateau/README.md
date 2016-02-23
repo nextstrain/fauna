b076e41920b37619f83e5e61747455edb3f3e57f
Trevor Bedford <trvrb@trevorbedford.com>
2016-02-22 21:53:02 -0800

    Include info on Chateau web UI.

---
+++ chateau/README.md
@@ -0,0 +1,13 @@
+# Chateau UI
+
+[Chateau](https://github.com/neumino/chateau) gives a more powerful UI to the underlying data than the built-in RethinkDB data explorer.
+
+To run, do the following:
+
+1. Install Chateau with `sudo npm install -g chateau`.
+
+2. Navigate to `vdb/chateau/`.
+
+3. Run with `chateau -f config.js`.
+
+4. Fire up browser and go to `http://localhost:3000/`.
---
+++ chateau/config.js
@@ -0,0 +1,9 @@
+// RethinkDB settings
+exports.host = 'ec2-52-90-204-136.compute-1.amazonaws.com';    	// RethinkDB host
+exports.port = 28015;          									// RethinkDB driver port
+exports.authKey = 'KeHiybPoX8BM6aAhfYqy';   					// Authentification key (leave an empty string if you did not set one)
+
+// Express settings
+exports.expressPort = 3000;    // Port used by express
+exports.debug = true;          // Debug mode
+exports.network = '127.0.0.1'  // Network the node app will run on
