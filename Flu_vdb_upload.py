0c58050143a76c535e7b0a09320e251f66db4034
Charlton Callender <chacalle@uw.edu>
2016-02-26 09:58:36 -0800

    Camelcase country names from geo_synonyms

--- Flu_vdb_upload.py
+++ Flu_vdb_upload.py
@@ -14,35 +14,6 @@ class Flu_vdb_upload(vdb_upload):
         self.virus_optional_fields = ['division', 'location']
         vdb_upload.__init__(self, fasta_fields, fasta_fname, database, virus, source, locus, vsubtype, authors, path, auth_key)
 
-    def format(self):
-        '''
-        format virus information in preparation to upload to database table
-        '''
-        print('Formatting for upload')
-        self.define_countries()
-        self.define_regions()
-        for virus in self.viruses:
-            self.format_sequence_schema(virus)
-            self.format_date(virus)
-            self.format_country(virus)
-            self.format_region(virus)
-            self.format_place(virus)
-
-        # filter out viruses without correct dating format or without region specified
-        self.viruses = filter(lambda v: re.match(r'\d\d\d\d-(\d\d|XX)-(\d\d|XX)', v['date']), self.viruses)
-        self.viruses = filter(lambda v: v['region'] != 'Unknown', self.viruses)
-        self.check_all_attributes()
-
-    def define_countries(self):
-        '''
-        open synonym to country dictionary
-        Location is to the level of country of administrative division when available
-        '''
-        reader = csv.DictReader(open("source-data/geo_synonyms.tsv"), delimiter='\t')		# list of dicts
-        self.label_to_country = {}
-        for line in reader:
-            self.label_to_country[line['label'].lower()] = line['country']
-
     def format_country(self, v):
         '''
         Label viruses with country based on strain name
