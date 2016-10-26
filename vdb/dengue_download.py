8b7120c3d840ca51894b4a80cfb4070cf4e180b0
sidneymbell <sidneymb@uw.edu>
2016-10-26 14:15:39 -0500

    Add support for lists of loci.

--- vdb/dengue_download.py
+++ vdb/dengue_download.py
@@ -6,12 +6,25 @@ class dengue_download(download):
     def __init__(self, **kwargs):
         download.__init__(self, **kwargs)
 
+    def add_selections_command(self, command, selections=[], **kwargs): # Command is an instance of r.table
+        '''
+        Add selections filter to command
+        '''
+        if len(selections)>0:
+            for sel in selections: # sel like (field, [value1, value2, ...])
+                field = sel[0]
+                values = sel[1]
+                if field == 'locus':
+                    print "Only downloading documents with one or more of %s in 'locus' field."%str(values)
+                    command = command.filter(lambda doc: doc[field] in values)
+                else:
+                    print("Only downloading documents with field \'" + field + "\' equal to one of " + str(values))
+                    command = command.filter(lambda doc: r.expr(values).contains(doc[field]))
+        return command
+
 if __name__=="__main__":
     parser = get_parser()
     args = parser.parse_args()
-    # fasta_fields = ['strain', 'virus', 'accession', 'collection_date', 'region',
-    #                 'country', 'division', 'location', 'source', 'locus',
-    #                 'authors', 'latitude', 'longitude']
     fasta_fields = ['strain', 'accession']
 
     args.fasta_fields = fasta_fields
