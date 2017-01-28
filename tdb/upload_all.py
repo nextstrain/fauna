dd741877dde913598e3331522f0783b73daa6b4d
Barney Potter <barney.potter.24@gmail.com>
2017-01-27 16:55:06 -0800

    Make upload_all generalizable across directory structures.

--- tdb/upload_all.py
+++ tdb/upload_all.py
@@ -1,95 +1,67 @@
-#
-# To use this upload script, fill in the paths containing NIMR, CDC, eLife, and stored
-# titer tables under the appropriate variables in main marked with "TODO:"
-#
-
 import subprocess
 import os
-import shutil
 
-def upload_nimr(database, directory):
-    # Upload NIMR tables (September Report)
+parser = argparse.ArgumentParser()
+parser.add_argument('--db', '--database', default='test_tdb_2', help="database to upload to")
+parser.add_argument('--subtype', default='h3n2', help="subtype to upload")
+parser.add_argument('--nimr_path', default='data/nimr/', help="directory containing NIMR titers")
+parser.add_argument('--cdc_path', default='data/cdc/', help="directory containing CDC titers")
+parser.add_argument('--elife_path', default='data/elife/', help="directory containing eLife titers")
+
+def upload_nimr(database, directory, nimr_path, subtype):
+    # Upload NIMR tables
     print "Beginning upload of NIMR documents to", database + "."
-    subtypes = ["h3n2", "h1n1pdm", "vic", "yam"]
-    for subtype in subtypes:
-        path = "data/" + subtype + "/"
-        print "Uploading NIMR reports for subtype", subtype, "contained in directory", path + "."
+    path = nimr_path + subtype + "/"
+    print "Uploading NIMR reports for subtype", subtype, "contained in directory", path + "."
 
-        for fname in os.listdir(path):
-            if fname [0] == "N":
-                fpath = directory + path + fname
-                fstem = fname[:-4]
-                command = "python tdb/nimr_upload.py -db " + database + " --subtype " + subtype + " --ftype tables --path " + path + " --fstem " + fstem
-                subprocess.call(command, shell=True)
-                print "Done with", fname + "."
+    for fname in os.listdir(path):
+        fpath = path + fname
+        fstem = fname[:-4]
+        command = "python tdb/nimr_upload.py -db " + database + " --subtype " + subtype + " --ftype tables --path " + path + " --fstem " + fstem
+        subprocess.call(command, shell=True)
+        print "Done with", fname + "."
 
     print "Done uploading NIMR documents."
 
-def upload_cdc(database, directory):
+def upload_cdc(database, cdc_path, subtype):
     # Upload CDC flat files
     print "Beginning upload of CDC documents to", database + "."
-    subtypes = ["h3n2"]
-    for subtype in subtypes:
-        path = directory
-        print "Uploading CDC reports for subtype", subtype, "contained in directory", path + "."
+    path = cdc_path + subtype + "/"
+    print "Uploading CDC reports for subtype", subtype, "contained in directory", path + "."
 
-        for fname in os.listdir(path):
-            if fname [0:3] == "test":
-                fpath = path + fname
-                fstem = fname[:-4]
-                command = "python tdb/cdc_upload.py -db " + database + " --subtype " + subtype + " --path " + path + "tmp/ --fstem " + fstem
-                subprocess.call(command, shell=True)
-                print "Done with", fname + "."
+    for fname in os.listdir(path):
+        fpath = path + fname
+        fstem = fname[:-4]
+        command = "python tdb/cdc_upload.py -db " + database + " --subtype " + subtype + " --path " + path + " --fstem " + fstem
+        subprocess.call(command, shell=True)
+        print "Done with", fname + "."
 
     print "Done uploading CDC documents."
 
-def upload_stored(database, directory):
-    # Upload stored NIMR files
-    print "Beginning upload of stored NIMR documents to", database + "."
-    subtypes = ["h3n2", "h1n1pdm", "vic", "yam"]
-    for subtype in subtypes:
-        path = directory + subtype + "/"
-        print "Uploading CDC reports for subtype", subtype, "contained in directory", path + "."
-
-        for fname in os.listdir(path):
-            if fname [0] == "N":
-                fpath = path + fname
-                fstem = fname[:-4]
-                command = "python tdb/nimr_upload.py -db " + database + " --subtype " + subtype + " --ftype tables --path " + path + "tmp/"
-                subprocess.call(command, shell=True)
-                print "Done with", fname + "."
-
-    print "Done uploading stored NIMR documents."
-
-def upload_elife(database, directory):
+def upload_elife(database, elife_path, subtype):
     # Upload stored elife files
     print "Beginning upload of stored NIMR documents to", database + "."
-    subtypes = ["h3n2", "vic", "yam"]
-    for subtype in subtypes:
-        path = directory + subtype + "/"
-        print "Uploading CDC reports for subtype", subtype, "contained in directory", path + "."
+    path = elife_path + subtype + "/"
+    print "Uploading CDC reports for subtype", subtype, "contained in directory", path + "."
 
-        for fname in os.listdir(path):
-            if fname [0] == "b":
-                fpath = path + fname
-                fstem = fname[:-4]
-                command = "python tdb/elife_upload.py -db " + database + " --subtype " + subtype + " --path " + path + "tmp/ --fstem " + fstem
-                subprocess.call(command, shell=True)
-                print "Done with", fname + "."
+    for fname in os.listdir(path):
+        fpath = path + fname
+        fstem = fname[:-4]
+        command = "python tdb/elife_upload.py -db " + database + " --subtype " + subtype + " --path " + path + " --fstem " + fstem
+        subprocess.call(command, shell=True)
+        print "Done with", fname + "."
 
     print "Done uploading stored eLife documents."
 
-
-
 if __name__=="__main__":
-    db = #TODO: Fill this in.
-    nimr_dir = #TODO: Fill this in.
-    cdc_dir = #TODO: Fill this in.
-    stored_dir = #TODO: Fill this in.
-    elife_dir = #TODO: Fill this in.
+    args = parser.parse_args()
     print "Beginning construction of", db + "."
-    upload_nimr(db, nimr_dir)
-    upload_cdc(db, cdc_dir)
-    upload_stored(db, stored_dir)
-    upload_elife(db, elife_dir)
+    upload_nimr(db, args.nimr_dir, "h3n2")
+    upload_nimr(db. args.nimr_dir, "h1n1pdm")
+    upload_nimr(db. args.nimr_dir, "vic")
+    upload_nimr(db. args.nimr_dir, "yam")
+    upload_cdc(db, args.cdc_dir, "h3n2")
+    upload_elife(db, args.elife_dir, "h3n2")
+    upload_elife(db, args.elife_dir, "vic")
+    upload_elife(db, args.elife_dir, "yam")
     print "Done with all uploads."
