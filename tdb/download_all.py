e9864e20d6bf208fa28647a407168dce32c66c70
Richard Neher <richard.neher@tuebingen.mpg.de>
2017-02-24 13:08:06 +0100

    restructured download all, booleans should become flags

--- tdb/download_all.py
+++ tdb/download_all.py
@@ -8,43 +8,50 @@
 # sister directories to fauna/
 
 import os
+copy_data = False
+sequences=False
+crick=False
+cdc=True
 
 # Download FASTAs from database
-os.system("python vdb/flu_download.py -db vdb -v flu --select locus:HA lineage:seasonal_h3n2 --fstem h3n2")
-os.system("python vdb/flu_download.py -db vdb -v flu --select locus:HA lineage:seasonal_h1n1pdm --fstem h1n1pdm")
-os.system("python vdb/flu_download.py -db vdb -v flu --select locus:HA lineage:seasonal_vic --fstem vic")
-os.system("python vdb/flu_download.py -db vdb -v flu --select locus:HA lineage:seasonal_yam --fstem yam")
+if sequences:
+    for lineage in ['h3n2', 'h1n1pdm', 'vic', 'yam']:
+        call = "python vdb/flu_download.py -db vdb -v flu --select locus:HA lineage:seasonal_%s --fstem %s"%(lineage, lineage)
+        os.system(call)
 
 # Copy FASTAs to nextflu/augur directory. Leave in fauna/data/ for nextstrain/augur.
-os.system("cp data/h3n2.fasta ../nextflu/augur/data/")
-os.system("cp data/h1n1pdm.fasta ../nextflu/augur/data/")
-os.system("cp data/vic.fasta ../nextflu/augur/data/")
-os.system("cp data/yam.fasta ../nextflu/augur/data/")
+if copy_data:
+    os.system("cp data/h3n2.fasta ../nextflu/augur/data/")
+    os.system("cp data/h1n1pdm.fasta ../nextflu/augur/data/")
+    os.system("cp data/vic.fasta ../nextflu/augur/data/")
+    os.system("cp data/yam.fasta ../nextflu/augur/data/")
 
-# Download Crick titers from database
-os.system("python tdb/download.py -db tdb -v flu --subtype h3n2 --select assay_type:hi --fstem h3n2_crick_hi")
-os.system("python tdb/download.py -db tdb -v flu --subtype h1n1pdm --select assay_type:hi --fstem h1n1pdm_crick_hi")
-os.system("python tdb/download.py -db tdb -v flu --subtype vic --select assay_type:hi --fstem vic_crick_hi")
-os.system("python tdb/download.py -db tdb -v flu --subtype yam --select assay_type:hi --fstem yam_crick_hi")
+if crick:
+    for lineage in ['h3n2', 'h1n1pdm', 'vic', 'yam']:
+        call = "python tdb/download.py -db tdb -v flu --subtype %s --select assay_type:hi --fstem %s_crick_hi"%(lineage, lineage)
+        os.system(call)
 
-# Copy TSVs to nextflu/augur directory. Leave in fauna/data/ for nextstrain/augur.
-os.system("cp data/h3n2_crick_hi_strains.tsv ../nextflu/augur/data/h3n2_hi_strains.tsv")
-os.system("cp data/h3n2_crick_hi_titers.tsv ../nextflu/augur/data/h3n2_hi_titers.tsv")
-os.system("cp data/h1n1pdm_crick_hi_strains.tsv ../nextflu/augur/data/h1n1pdm_hi_strains.tsv")
-os.system("cp data/h1n1pdm_crick_hi_titers.tsv ../nextflu/augur/data/h1n1pdm_hi_titers.tsv")
-os.system("cp data/vic_crick_hi_strains.tsv ../nextflu/augur/data/vic_hi_strains.tsv")
-os.system("cp data/vic_crick_hi_titers.tsv ../nextflu/augur/data/vic_hi_titers.tsv")
-os.system("cp data/yam_crick_hi_strains.tsv ../nextflu/augur/data/yam_hi_strains.tsv")
-os.system("cp data/yam_crick_hi_titers.tsv ../nextflu/augur/data/yam_hi_titers.tsv")
 
 # Download CDC HI titers from database
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype h3n2 --select assay_type:hi --fstem h3n2_cdc_hi")
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype h1n1pdm --select assay_type:hi --fstem h1n1pdm_cdc_hi")
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype vic --select assay_type:hi --fstem vic_cdc_hi")
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype yam --select assay_type:hi --fstem yam_cdc_hi")
+if cdc:
+    for lineage in ['h3n2', 'h1n1pdm', 'vic', 'yam']:
+        for passage in ["egg", "cell"]:
+            call = "python tdb/download.py -db cdc_tdb -v flu --subtype %s --select assay_type:hi serum_passage_category:%s --fstem %s_cdc_hi_%s"%(lineage, passage, lineage, passage)
+            os.system(call)
 
-# Download CDC FRA titers from database
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype h3n2 --select assay_type:fra --fstem h3n2_cdc_fra")
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype h1n1pdm --select assay_type:fra --fstem h1n1pdm_cdc_fra")
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype vic --select assay_type:fra --fstem vic_cdc_fra")
-os.system("python tdb/download.py -db cdc_tdb -v flu --subtype yam --select assay_type:fra --fstem yam_cdc_fra")
+    # Download CDC FRA titers from database
+    lineage = 'h3n2'
+    for passage in ["egg", "cell"]:
+        call = "python tdb/download.py -db cdc_tdb -v flu --subtype %s --select assay_type:fra serum_passage_category:%s --fstem %s_cdc_fra_%s"%(lineage, passage, lineage, passage)
+        os.system(call)
+
+# Copy TSVs to nextflu/augur directory. Leave in fauna/data/ for nextstrain/augur.
+if copy_data:
+    os.system("cp data/h3n2_crick_hi_strains.tsv ../nextflu/augur/data/h3n2_hi_strains.tsv")
+    os.system("cp data/h3n2_crick_hi_titers.tsv ../nextflu/augur/data/h3n2_hi_titers.tsv")
+    os.system("cp data/h1n1pdm_crick_hi_strains.tsv ../nextflu/augur/data/h1n1pdm_hi_strains.tsv")
+    os.system("cp data/h1n1pdm_crick_hi_titers.tsv ../nextflu/augur/data/h1n1pdm_hi_titers.tsv")
+    os.system("cp data/vic_crick_hi_strains.tsv ../nextflu/augur/data/vic_hi_strains.tsv")
+    os.system("cp data/vic_crick_hi_titers.tsv ../nextflu/augur/data/vic_hi_titers.tsv")
+    os.system("cp data/yam_crick_hi_strains.tsv ../nextflu/augur/data/yam_hi_strains.tsv")
+    os.system("cp data/yam_crick_hi_titers.tsv ../nextflu/augur/data/yam_hi_titers.tsv")
