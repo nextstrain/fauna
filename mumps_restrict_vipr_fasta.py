311560f80b408a91afc8cb51d71fe93cd6f21bd6
James Hadfield <jh22@sanger.ac.uk>
2017-07-06 16:55:10 -0700

    mumps preprocessing

--- mumps_restrict_vipr_fasta.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-with open("data/mumps_vipr_full.fasta", 'rU') as fh:
-    seqs = [x for x in SeqIO.parse(fh, 'fasta') if 'MuV' in x.name]
-for seq in seqs:
-    seq.name = seq.name.replace('[','(').replace(']',')')
-    seq.description = seq.name
-    seq.id = seq.name
-with open("data/mumps_vipr.fasta", 'w') as fh:
-    SeqIO.write(seqs, fh, "fasta")
---
+++ vdb/mumps_preprocess_fasta.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+from Bio import SeqIO
+import argparse
+import os, sys
+from pdb import set_trace
+
+def fixHeaders(seqs):
+    with open("source-data/mumps-name-fix.txt", 'rU') as fh:
+        fixes = {x.strip().split()[0]: x.strip().split()[1] for x in fh.readlines() if not x.startswith('#')}
+    for seq in seqs:
+        if seq.name in fixes:
+            print("seq.name:", seq.name, " -> ", fixes[seq.name])
+            seq.name = fixes[seq.name]
+    return seqs
+
+def standardiseViaName(seqs):
+    for seq in seqs:
+        seq.name = seq.name.replace('[','(').replace(']',')')
+        seq.description = seq.name
+        seq.id = seq.name
+    return seqs
+
+def vipr():
+    with open("data/mumps_vipr_full.fasta", 'rU') as fh:
+        seqs = [x for x in SeqIO.parse(fh, 'fasta') if 'MuV' in x.name]
+    seqs = fixHeaders(seqs)
+    seqs = standardiseViaName(seqs)
+    with open("data/mumps_vipr.fasta", 'w') as fh:
+        SeqIO.write(seqs, fh, "fasta")
+    print("Fixed VIPR fasta file saved to data/mumps_vipr.fasta")
+
+def preprocessFASTA(fname):
+    with open(fname, 'rU') as fh:
+        seqs = [x for x in SeqIO.parse(fh, 'fasta')]
+    seqs = fixHeaders(seqs)
+    seqs = standardiseViaName(seqs)
+    fname_out = fname + ".fixed.fasta"
+    with open(fname_out, 'w') as fh:
+        SeqIO.write(seqs, fh, "fasta")
+    print("Fixed FASTA and saved to ", fname_out)
+
+def collect_args():
+    parser = argparse.ArgumentParser(description = "Preprocess mumps FASTA for fauna upload")
+    parser.add_argument('--vipr', action='store_true', help='vipr fasta file')
+    parser.add_argument('--fasta', type=str, default="", help='other fasta file')
+    return parser.parse_args()
+
+if __name__=="__main__":
+    params = collect_args()
+    if params.vipr:
+        vipr()
+    elif params.fasta:
+        preprocessFASTA(params.fasta)
+
+    # elif params.
