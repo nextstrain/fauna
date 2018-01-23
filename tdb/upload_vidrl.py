591b06b1e86d11c55465767a6841cec581b2a56e
Barney Potter <barney.potter.24@gmail.com>
2018-01-23 15:59:46 -0800

    Fix systematic name typo and fix assay_type bug.

--- tdb/upload_vidrl.py
+++ tdb/upload_vidrl.py
@@ -3,36 +3,49 @@ from upload import upload
 import rethinkdb as r
 from Bio import SeqIO
 import argparse
+import subprocess
 from parse import parse
 from upload import parser
 sys.path.append('')  # need to import from base
 from base.rethink_io import rethink_io
 from vdb.flu_upload import flu_upload
+import logging
+logger = logging.getLogger()
 
-def read_virdl(path, fstem, **kwargs):
+def read_vidrl(path, fstem, **kwargs):
     '''
     Read all csv tables in path, create data frame with reference viruses as columns
     '''
     fname = path + fstem + ".csv"
     # import glob
     # flist = glob.glob(path + '/NIMR*csv') #BP
-    exten = [ os.path.isfile(path + fstem + ext) for ext in ['.xls', '.xlsm', 'xlsx'] ]
+    exten = [ os.path.isfile(path + fstem + ext) for ext in ['.xls', '.xlsm', '.xlsx'] ]
     if True in exten:
         ind = exten.index(True)
         convert_xls_to_csv(path, fstem, ind)
         fname = "data/tmp/%s.csv"%(fstem)
-        parse_virdl_matrix_to_tsv(fname, path)
+        parse_vidrl_matrix_to_tsv(fname, path)
+    else:
+        logger.critical("Unable to recognize file extension of {}/{}".format(path,fstem))
+        sys.exit()
 
 def convert_xls_to_csv(path, fstem, ind):
     import xlrd
-    exts = ['.xls', '.xlsm', 'xlsx']
+    exts = ['.xls', '.xlsm', '.xlsx']
     workbook = xlrd.open_workbook(path+fstem + exts[ind])
     for sheet in workbook.sheets():
         with open('data/tmp/%s.csv'%(fstem), 'wb') as f:
             writer = csv.writer(f)
-            writer.writerows(sheet.row_values(row) for row in range(sheet.nrows))
+            writer.writerows(sheet.row_values(row) for row in range(10))
+            # Edit row containing serum strains
+            temp = sheet.row_values(10)
+            col = sheet.col_values(2)
+            for i in range(4,16):
+                temp[i] = col[11+i-4]
+            writer.writerow(temp)
+            writer.writerows(sheet.row_values(row) for row in range(11,sheet.nrows))
 
-def parse_virdl_matrix_to_tsv(fname, original_path):
+def parse_vidrl_matrix_to_tsv(fname, original_path):
     from string import strip
     src_id = fname.split('/')[-1]
     with open(fname) as infile:
@@ -53,13 +66,22 @@ def parse_virdl_matrix_to_tsv(fname, original_path):
                 serum_strain = mat[10][j]
                 serum_id = mat[8][j]
                 titer = mat[i][j]
-                source = "VIRDL_%s"%(src_id)
+                source = "vidrl_%s"%(src_id)
                 virus_passage = mat[i][16]
                 virus_passage_category = ''
                 serum_passage = mat[9][j]
                 serum_passage_category = ''
-                line = "%s\n" % ("\t".join([ virus_strain, serum_strain, titer, source, virus_passage, virus_passage_category, serum_passage, serum_passage_category, assay_type]))
+                line = "%s\n" % ("\t".join([ virus_strain, serum_strain, serum_id, titer, source, virus_passage, virus_passage_category, serum_passage, serum_passage_category, assay_type]))
                 outfile.write(line)
+
+def determine_subtype(original_path):
+    original_path = original_path.split('/')
+    try:
+        original_path.remove('')
+    except:
+        pass
+    subtype = original_path[-2]
+    return subtype
 
 if __name__=="__main__":
     args = parser.parse_args()
@@ -67,9 +89,15 @@ if __name__=="__main__":
         args.path = "data/"
     if not os.path.isdir(args.path):
         os.makedirs(args.path)
-    read_virdl(args.path, args.fstem)
-    print "Done with that step!"
-    #TODO: This is where I will add conversion of VIRDL files to eLife format!
-    sys.exit()
-    connTDB = elife_upload(**args.__dict__)
-    connTDB.upload(**args.__dict__)
+    read_vidrl(args.path, args.fstem)
+    ####
+    subtype = determine_subtype(args.path)
+    #TODO: This is where I will add conversion of vidrl files to eLife format!
+    if args.preview:
+        command = "python tdb/elife_upload.py -db vidrl_tdb --subtype " + subtype + " --path data/tmp/ --fstem " + args.fstem + " --preview"
+        print command
+        subprocess.call(command, shell=True)
+    else:
+        command = "python tdb/elife_upload.py -db vidrl_tdb --subtype " + subtype + " --path data/tmp/ --fstem " + args.fstem
+        print command
+        subprocess.call(command, shell=True)
