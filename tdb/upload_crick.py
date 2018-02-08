d591dc69092602b6e81bae2861b88f20cdbdda41
Barney Potter <barney.potter.24@gmail.com>
2018-02-08 11:42:27 -0800

    Add assay_type flag and get crick uploads functional.

--- tdb/upload_crick.py
+++ tdb/upload_crick.py
@@ -14,37 +14,50 @@ from vdb.flu_upload import flu_upload
 # logger = logging.getLogger()
 # print 'more yay'
 
-def read_crick(path, fstem):
+parser.add_argument('--assay_type', default='hi')
+
+def read_crick(path, fstem, assay_type):
     '''
     Read all csv tables in path, create data frame with reference viruses as columns
     '''
-    fname = path + fstem + ".csv"
+    fname = path + fstem # + ".csv"
     # import glob
     # flist = glob.glob(path + '/NIMR*csv') #BP
     exten = [ os.path.isfile(path + fstem + ext) for ext in ['.xls', '.xlsm', '.xlsx'] ]
     if True in exten:
         ind = exten.index(True)
-        convert_xls_to_csv(path, fstem, ind)
-        fname = "data/tmp/%s.csv"%(fstem)
-        parse_crick_matrix_to_tsv(fname, path)
+        sheets = convert_xls_to_csv(path, fstem, ind)
+        for sheet in sheets:
+            fname = "data/tmp/{}.csv".format(sheet)
+            parse_crick_matrix_to_tsv(fname, path, assay_type)
     else:
         # logger.critical("Unable to recognize file extension of {}/{}".format(path,fstem))
+        print("EXITING")
         sys.exit()
+    return sheets
 
 def convert_xls_to_csv(path, fstem, ind):
     import xlrd
+    sheets = []
     exts = ['.xls', '.xlsm', '.xlsx']
     workbook = xlrd.open_workbook(path+fstem + exts[ind])
     for sheet in workbook.sheets():
-        with open('data/tmp/%s.csv'%(fstem), 'wb') as f:
-            writer = csv.writer(f)
-            writer.writerows(sheet.row_values(row) for row in range(6))
-            writer.writerow(temp)
-            writer.writerows(sheet.row_values(row) for row in range(11,sheet.nrows))
-        sys.exit()
-        return
+        with open('data/tmp/{}_{}.csv'.format(fstem, sheet.name), 'wb') as f:
+            try:
+                writer = csv.writer(f)
+                print(sheet.name)
+                writer.writerows( sheet.row_values(row) for row in range(sheet.nrows))
+                # writer.writerow(temp)
+                # writer.writerows(sheet.row_values(row) for row in range(11,sheet.nrows))
+            except:
+                print("couldn't write data/tmp/{}_{}.csv".format(fstem,sheet.name))
+        print("wrote new csv to data/tmp/{}_{}.csv".format(fstem, sheet.name))
+        sheets.append("{}_{}".format(fstem, sheet.name))
+        # sys.exit()
+    return sheets
 
-def parse_crick_matrix_to_tsv(fname, original_path):
+def parse_crick_matrix_to_tsv(fname, original_path, assay_type):
+    assay_type = assay_type
     from string import strip
     src_id = fname.split('/')[-1]
     with open(fname) as infile:
@@ -58,33 +71,34 @@ def parse_crick_matrix_to_tsv(fname, original_path):
             original_path.remove('')
         except:
             pass
-        assay_type = original_path[-1]
-        for i in range(12,len(mat)):
-            for j in range(4,15):
-                virus_strain = mat[i][2]
-                serum_strain = mat[10][j]
-                serum_id = mat[8][j]
+        for i in range(14,len(mat)):
+            for j in range(7,len(mat[0])):
+                # Change this line to mat[i][2] for non-FRA tables
+                virus_strain = mat[i][1]
+                serum_strain = mat[6][j]
+                serum_id = mat[9][j]
                 titer = mat[i][j]
                 source = "crick_%s"%(src_id)
-                virus_passage = mat[i][16]
+                virus_passage = mat[i][6]
                 virus_passage_category = ''
-                serum_passage = mat[9][j]
+                serum_passage = mat[8][j]
                 serum_passage_category = ''
                 line = "%s\n" % ("\t".join([ virus_strain, serum_strain, serum_id, titer, source, virus_passage, virus_passage_category, serum_passage, serum_passage_category, assay_type]))
                 outfile.write(line)
 
-def determine_subtype(original_path):
-    original_path = original_path.split('/')
-    try:
-        original_path.remove('')
-    except:
-        pass
-    subtype = original_path[-2]
-    if subtype.lower() == "victoria":
-        subtype = "vic"
-    if subtype.upper() == "yamagata":
-        subtype = "yam"
+def determine_subtype(fname):
+    if fname.lower().startswith('h3n2'):
+        subtype = 'h3n2'
+    elif fname.lower().startswith ('h1n1pdm'):
+        subtype = 'h1n1pdm'
+    elif fname.lower().startswith('bvic'):
+        subtype = 'vic'
+    elif fname.lower().startswith('byam'):
+        subtype = 'yam'
+    else:
+        subtype = 'unknown'
     return subtype
+
 
 if __name__=="__main__":
     args = parser.parse_args()
@@ -93,15 +107,17 @@ if __name__=="__main__":
     if not os.path.isdir(args.path):
         os.makedirs(args.path)
     # x_shift, y_shift = determine_initial_indices(args.path, args.fstem)
-    read_crick(args.path, args.fstem)
-    ####
-    subtype = determine_subtype(args.path)
+    sheets = read_crick(args.path, args.fstem, args.assay_type)
     #TODO: This is where I will add conversion of crick files to eLife format!
-    if args.preview:
-        command = "python tdb/elife_upload.py -db crick_tdb --subtype " + subtype + " --path data/tmp/ --fstem " + args.fstem + " --preview"
-        print command
-        subprocess.call(command, shell=True)
-    else:
-        command = "python tdb/elife_upload.py -db crick_tdb --subtype " + subtype + " --path data/tmp/ --fstem " + args.fstem
-        print command
-        subprocess.call(command, shell=True)
+    for sheet in sheets:
+        subtype = determine_subtype(sheet)
+        if args.preview:
+            print("Subtype: {}".format(subtype))
+            print("Sheet: {}".format(sheet))
+            command = "python tdb/elife_upload.py -db crick_tdb --subtype " + subtype + " --path data/tmp/ --fstem " + sheet + " --preview"
+            print command
+            subprocess.call(command, shell=True)
+        else:
+            command = "python tdb/elife_upload.py -db crick_tdb --subtype " + subtype + " --path data/tmp/ --fstem " + sheet
+            print command
+            subprocess.call(command, shell=True)
