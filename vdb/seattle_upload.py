7f092940aa75db8216f04a9349b35dfdfe875c2b
Trevor Bedford <trevor@bedford.io>
2019-05-26 13:28:22 -0600

    Revise Seattle upload

--- vdb/seattle_upload.py
+++ vdb/seattle_upload.py
@@ -11,10 +11,10 @@ class seattle_upload(upload):
 if __name__=="__main__":
     parser = get_parser()
     args = parser.parse_args()
-    virus_fasta_fields = {0:'strain', 2:'type', 4:'collection_date', 5:'country', 6:'division', 7:'division'}
+    virus_fasta_fields = {0:'strain', 2:'type'}
     sequence_fasta_fields = {0:'strain', 1:'accession', 3:'segment'}
-    # 0            1                2    3   4          5   6          7
-    #>FS_10_S1_001|FS_10_S1_001-PB1|H3N2|PB1|XXXX-XX-XX|usa|washington|seattle
+    # 0                                    1                                       2    3
+    #>87676fe4-1bf2-4321-ba09-27d81ced2508|87676fe4-1bf2-4321-ba09-27d81ced2508-HA|H3N2|HA
     setattr(args, 'virus_fasta_fields', virus_fasta_fields)
     setattr(args, 'sequence_fasta_fields', sequence_fasta_fields)
     connVDB = seattle_upload(**args.__dict__)
