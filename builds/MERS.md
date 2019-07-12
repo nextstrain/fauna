241aad683186b1352b3b9fea1ee57f80536444f0
Trevor Bedford <trevor@bedford.io>
2019-07-12 15:34:31 -0700

    First draft of MERS upload/download scripts

---
+++ builds/MERS.md
@@ -0,0 +1,15 @@
+## Download data from Genbank
+
+
+
+Start from the existing accession list from Dudas et al. 2018: https://github.com/nextstrain/mers/blob/master/data/MERS_CoV_274_CDS.dates.txt
+
+Call this file `mers_accessions.txt` and put it in the `fauna/data/` directory.
+
+## Upload to fauna
+
+`python2 vdb/mers_upload.py -db test_vdb -v mers --ftype accession --source genbank --locus genome --fname mers_accessions.txt`
+
+## Download from fauna
+
+`python2 vdb/mers_download.py -db test_vdb -v mers --fstem measles --resolve_method choose_genbank`
---
+++ vdb/mers_upload.py
@@ -0,0 +1,41 @@
+import os, re, time, datetime, csv, sys
+import rethinkdb as r
+from Bio import SeqIO
+from upload import upload
+from upload import get_parser
+
+class mers_upload(upload):
+    def __init__(self, **kwargs):
+        upload.__init__(self, **kwargs)
+        self.strain_fix_fname = "source-data/measles_strain_name_fix.tsv"
+        self.location_fix_fname = "source-data/measles_location_fix.tsv"
+        self.date_fix_fname = "source-data/measles_date_fix.tsv"
+
+    def fix_name(self, name):
+        original_name = name
+        print("original name", original_name)
+        name = self.replace_strain_name(original_name, self.fix_whole_name)
+        name = name.replace('MVs/', '').replace('MVi/', '').replace('Mvi/', '')
+        name = re.sub(r'[_ ]?\[([A-Z][0-9])\]$', r'/\1', name)
+        name = re.sub(r'\(([A-Z][0-9])\)$', r'/\1', name)
+        name = re.sub(r'_([A-Z][0-9])_$', r'/\1', name)
+        name = re.sub(r'[ ;]', r'_', name)
+        name = re.sub(r'//', r'/', name)
+        name = self.replace_strain_name(name, self.fix_whole_name)
+        print("fixed name", name)
+        return name, original_name
+
+    def fix_casing(self, document):
+        for field in ['host']:
+            if field in document and document[field] is not None:
+                document[field] = self.camelcase_to_snakecase(document[field])
+
+if __name__=="__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    virus_fasta_fields = {1:'strain', 2:'collection_date', 3: 'host', 4:'country', 5:'division'}
+    sequence_fasta_fields = {0:'accession', 1:'strain'}
+    setattr(args, 'virus_fasta_fields', virus_fasta_fields)
+    setattr(args, 'sequence_fasta_fields', sequence_fasta_fields)
+    connVDB = mers_upload(**args.__dict__)
+    connVDB.upload(**args.__dict__)
