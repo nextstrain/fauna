17ff08775f45516269e30e3229b4e9de0529b4e6
Jover <joverlee521@gmail.com>
2020-01-07 11:06:38 -0800

    tdb/cdc_upload: add `define_location_label_fixes`
    This was added/renamed in `vdb/flu_upload` and needed to be updated
    in `cdc_upload`.

--- tdb/cdc_upload.py
+++ tdb/cdc_upload.py
@@ -42,7 +42,7 @@ class cdc_upload(upload):
         '''
         self.fix_whole_name = self.define_strain_fixes(self.strain_fix_fname)
         self.HI_ref_name_abbrev =self.define_strain_fixes(self.HI_ref_name_abbrev_fname)
-        self.define_location_fixes("source-data/flu_fix_location_label.tsv")
+        self.define_location_label_fixes("source-data/flu_fix_location_label.tsv")
         self.define_countries("source-data/geo_synonyms.tsv")
         for meas in measurements:
             meas['source'] = 'cdc'
